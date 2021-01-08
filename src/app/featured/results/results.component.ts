239b2580086e1f47a1ca129ff6a885b3dd6c3b13
Alexandros Kontos <alexiskon01@gmail.com>
2021-01-08 02:17:01 +0200

    unsubscribe

--- src/app/featured/results/results.component.ts
+++ src/app/featured/results/results.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { error } from '@angular/compiler/src/util';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ResultsserviceService } from './services/resultsservice.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent implements OnInit, OnDestroy  {
 
   constructor(private getdata: ResultsserviceService) { }
 
@@ -20,6 +21,7 @@ export class ResultsComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
   //------------initialize angular material table---------\\
 
+  subs: Subscription = new Subscription();
   data = [];
   selectedYear: string = "2020";
   selectedRound: string = "1";
@@ -44,19 +46,19 @@ export class ResultsComponent implements OnInit {
   ngOnInit(): void {
 
     this.initialize();
-    this.getdata.getData(this.selectedYear, this.selectedRound).subscribe(value => {
+    this.subs.add(this.getdata.getData(this.selectedYear, this.selectedRound).subscribe(value => {
       this.data = value.MRData.RaceTable.Races[0].Results
       this.info1 = `${value.MRData.RaceTable.season}, Round ${value.MRData.RaceTable.round}, Race Results`;
       this.info2 = `${value.MRData.RaceTable.Races[0].Circuit.Location.country}, ${value.MRData.RaceTable.Races[0].Circuit.circuitName},  ${value.MRData.RaceTable.Races[0].date}`;
       this.dataSource = new MatTableDataSource(this.data)
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
-    })
+    }))
   }
 
   roundInput() {
     this.selectedRound = (document.getElementById('round') as HTMLInputElement).value.toString()
-    this.getdata.getData(this.selectedYear, this.selectedRound).subscribe(value => {
+    this.subs.add(this.getdata.getData(this.selectedYear, this.selectedRound).subscribe(value => {
       if (value.MRData.RaceTable.Races.length === 0) {
         this.roundControl = true;
       } else {
@@ -68,12 +70,12 @@ export class ResultsComponent implements OnInit {
       this.dataSource = new MatTableDataSource(this.data)
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
-    })
+    }))
   }
 
   yearInput() {
     this.selectedYear = (document.getElementById('year') as HTMLInputElement).value.toString()
-    this.getdata.getData(this.selectedYear, this.selectedRound).subscribe(value => {
+    this.subs.add(this.getdata.getData(this.selectedYear, this.selectedRound).subscribe(value => {
       if (value.MRData.RaceTable.Races.length === 0) {
         this.roundControl = true;
       } else {
@@ -86,6 +88,12 @@ export class ResultsComponent implements OnInit {
       this.dataSource = new MatTableDataSource(this.data)
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
-    })
+    }))
+  }
+
+  ngOnDestroy(): void {
+    if (this.subs) {
+      this.subs.unsubscribe();
+    }
   }
 }
--- src/app/featured/winners/winners.component.ts
+++ src/app/featured/winners/winners.component.ts
@@ -1,8 +1,9 @@
-import { ViewChild } from '@angular/core';
+import { OnDestroy, ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
 import { GetdatawinnersService } from './services/getdatawinners.service';
 
 @Component({
@@ -10,46 +11,53 @@ import { GetdatawinnersService } from './services/getdatawinners.service';
   templateUrl: './winners.component.html',
   styleUrls: ['./winners.component.scss']
 })
-export class WinnersComponent implements OnInit {
+export class WinnersComponent implements OnInit, OnDestroy {
 
   constructor(private getdata: GetdatawinnersService) { }
 
-   //------------initialize angular material table---------\\
-   displayedColumns: string[] = ['position', 'points', 'driverName', 'constructorName'];
-   dataSource = new MatTableDataSource();
-   @ViewChild(MatPaginator) paginator: MatPaginator;
-   @ViewChild(MatSort) sort: MatSort;
-   //------------initialize angular material table---------\\
- 
-   data = [];
-   displayedYear: string = "2020";
-   //-------------error control values------------\\
-   yearcontrol: boolean = false;
-   yearcontrolvalue: number;
- //-------------error control values------------\\
+  //------------initialize angular material table---------\\
+  displayedColumns: string[] = ['position', 'points', 'driverName', 'constructorName'];
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  //------------initialize angular material table---------\\
+  
+  subs: Subscription = new Subscription();
+  data = [];
+  displayedYear: string = "2020";
+  //-------------error control values------------\\
+  yearcontrol: boolean = false;
+  yearcontrolvalue: number;
+  //-------------error control values------------\\
 
   ngOnInit(): void {
-    this.getdata.getData(this.displayedYear).subscribe(value => {
+    this.subs.add(this.getdata.getData(this.displayedYear).subscribe(value => {
       this.data = value.MRData.StandingsTable.StandingsLists[0].DriverStandings
       this.dataSource = new MatTableDataSource(this.data)
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
-    })
+    }))
   }
   yearInput() {
     this.displayedYear = (document.getElementById('year-input') as HTMLInputElement).value
     this.yearcontrolvalue = parseInt(this.displayedYear)
-    if ( (this.yearcontrolvalue < 1950) || (this.yearcontrolvalue > 2020) ) {
+    if ((this.yearcontrolvalue < 1950) || (this.yearcontrolvalue > 2020)) {
       this.yearcontrol = true;
       return;
     }
-    this.getdata.getData(this.displayedYear).subscribe(value => {
+    this.subs.add(this.getdata.getData(this.displayedYear).subscribe(value => {
       this.data = value.MRData.StandingsTable.StandingsLists[0].DriverStandings
       this.dataSource = new MatTableDataSource(this.data)
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
       this.yearcontrol = false;
-    })
+    }))
+  }
+
+  ngOnDestroy(): void {
+    if (this.subs) {
+      this.subs.unsubscribe();
+    }
   }
 
 }
